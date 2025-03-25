79d83e0db549d3bcb8f09406d2d3e25d066f7140
ashwinKumar0505 <ashwin.gps@gmail.com>
2025-03-25 22:32:35 +0530

    Updated the animation

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -4,49 +4,73 @@ import Particles from "react-tsparticles";
 function Particle() {
   return (
     <Particles
-      id="tsparticles" 
-      params={{
-        particles: {
-          number: {
-            value: 160,
-            density: {
-              enable: true,
-              value_area: 1500,
-            },
-          },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
-            },
-          },
-        },
+      id="tsparticles"
+      options={{
+        fpsLimit: 120,
         interactivity: {
           events: {
-            onclick: {
+            onClick: {
               enable: true,
               mode: "push",
             },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
           },
           modes: {
             push: {
-              particles_nb: 1,
+              quantity: 2,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
             },
           },
         },
-        retina_detect: true,
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 30,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
       }}
     />
   );
